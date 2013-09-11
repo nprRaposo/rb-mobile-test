8a1851b4ecb4078c063807dfceffb1f13ddb703c
nprraposo <nikoraposo@gmail.com>
2013-09-11 00:39:16 -0300

    Agregado caso de test para n objetivos, y para un objetivo no cumplido

--- rbuddies-test/src/com/example/rbuddies/activities/test/LevelTest.java
+++ rbuddies-test/src/com/example/rbuddies/activities/test/LevelTest.java
@@ -11,7 +11,7 @@ import com.example.rbuddies.model.RBuddiesRegister;
 public class LevelTest extends RBuddiesTest {
 
 	@Test
-	public void testLevelIsAcomplishedIfTheirGoalsAreAcomplished() {
+	public void testLevelIsAcomplishedIfHisGoalIsAcomplished() {
 		Level aLevel = new Level();
 		
 		RBuddiesRegister goalTrack = this.getRegister(0, 100, 1);
@@ -20,6 +20,46 @@ public class LevelTest extends RBuddiesTest {
 		Goal aGoal = new Goal(goalTrack);
 		
 		aLevel.addGoal(aGoal);
+		
+		aLevel.isAcomplished(aTrack);
+	}
+	
+	@Test
+	public void testLevelIsAcomplishedIfTheirGoalAreAcomplished() {
+		Level aLevel = new Level();
+		
+		RBuddiesRegister goalTrack = this.getRegister(0, 100, 1);
+		RBuddiesRegister goalTrack2 = this.getRegister(0, 101, 1);
+		RBuddiesRegister goalTrack3 = this.getRegister(0, 102, 1);
+		RBuddiesRegister aTrack = this.getRegister(1, 1, 2);
+		
+		Goal aGoal = new Goal(goalTrack);
+		Goal aGoal2 = new Goal(goalTrack2);
+		Goal aGoal3 = new Goal(goalTrack3);
+		
+		aLevel.addGoal(aGoal);
+		aLevel.addGoal(aGoal2);
+		aLevel.addGoal(aGoal3);
+		
+		aLevel.isAcomplished(aTrack);
+	}
+	
+	@Test
+	public void testLevelIsnotAcomplishedIfOneGoalAreNotAcomplished() {
+		Level aLevel = new Level();
+		
+		RBuddiesRegister goalTrack = this.getRegister(0, 100, 1);
+		RBuddiesRegister goalTrack2 = this.getRegister(5, 101, 1);
+		RBuddiesRegister goalTrack3 = this.getRegister(0, 102, 1);
+		RBuddiesRegister aTrack = this.getRegister(1, 1, 2);
+		
+		Goal aGoal = new Goal(goalTrack);
+		Goal aGoal2 = new Goal(goalTrack2);
+		Goal aGoal3 = new Goal(goalTrack3);
+		
+		aLevel.addGoal(aGoal);
+		aLevel.addGoal(aGoal2);
+		aLevel.addGoal(aGoal3);
 		
 		aLevel.isAcomplished(aTrack);
 	}
